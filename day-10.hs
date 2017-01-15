21525138d60da4d54fbfc37945411c9c5196e48d
Craig Bilner <craig.bilner@gmail.com>
2017-01-15 01:56:20 +0000

    Add day 10 p 2

--- day-10.hs
+++ day-10.hs
@@ -18,7 +18,12 @@ data Robot = Robot { high         :: Maybe Int
                    , low          :: Maybe Int
                    } deriving (Show)
 
-type Robots = M.Map Int Robot
+data Place =
+      PlaceR Robot
+    | PlaceB Int
+    deriving (Show)
+
+type Places = M.Map String Place
 
 valueGoes :: Regex
 valueGoes = mkRegex "value ([0-9]*) goes to bot ([0-9]*)"
@@ -26,22 +31,30 @@ valueGoes = mkRegex "value ([0-9]*) goes to bot ([0-9]*)"
 botGives :: Regex
 botGives = mkRegex "bot ([0-9]*) gives ([a-z]*) to ([a-z]*) ([0-9]*) and ([a-z]*) to ([a-z]*) ([0-9]*)"
 
-maybeAddRobotWithValue :: Destination -> Maybe Int -> Robots -> Robots
-maybeAddRobotWithValue (Bot n) cv rs =
-    case M.lookup n rs of
-    Nothing    -> M.insert n (Robot cv Nothing) rs
-    Just robot -> case cv of
-                  Nothing -> rs
-                  Just v  -> M.update (addValue v) n rs
-maybeAddRobotWithValue _       _ rs = rs
+rId :: Int -> String
+rId n = 'r' : show n
 
-addValue :: Int -> Robot -> Maybe Robot
-addValue v r =
+bId :: Int -> String
+bId n = 'b' : show n
+
+maybeAddRobotWithValue :: Destination -> Maybe Int -> Places -> Places
+maybeAddRobotWithValue (Bot n)    cv ps =
+    case M.lookup (rId n) ps of
+    Nothing             -> M.insert (rId n) (PlaceR (Robot cv Nothing)) ps
+    Just (PlaceR robot) -> case cv of
+                           Nothing -> ps
+                           Just v  -> M.update (addValue v) (rId n) ps
+maybeAddRobotWithValue (Output n) cv ps =
+    M.insert (bId n) (PlaceB $ fromMaybe 0 cv) ps
+
+addValue :: Int -> Place -> Maybe Place
+addValue v (PlaceR r) =
     case high r of
-    Nothing -> Just $ r { high = Just v }
+    Nothing -> Just $ PlaceR $ r { high = Just v }
     Just h  -> if v > h
-               then Just $ r { high = Just v, low = Just h }
-               else Just $ r { low = Just v }
+               then Just $ PlaceR $ r { high = Just v, low = Just h }
+               else Just $ PlaceR $ r { low = Just v }
+addValue v (PlaceB n) = Just $ PlaceB v
 
 stringToInstructions :: [String] -> (Int, (ChipType, Destination), (ChipType, Destination))
 stringToInstructions [from, cType1, toType1, toNum1, cType2, toType2, toNum2] =
@@ -56,8 +69,8 @@ stringToInstructions [from, cType1, toType1, toNum1, cType2, toType2, toNum2] =
     in
         (from', (cType1', toType1'), (cType2', toType2'))
 
-makeRobotsAndIns :: [String] -> Robots -> [Instruction] -> (Robots, [Instruction])
-makeRobotsAndIns s rs ins =
+makePlacesAndIns :: [String] -> Places -> [Instruction] -> (Places, [Instruction])
+makePlacesAndIns s ps ins =
     let
         (from, ins1, ins2) = stringToInstructions s
         f                  = flip maybeAddRobotWithValue Nothing
@@ -65,29 +78,29 @@ makeRobotsAndIns s rs ins =
                              . (f $ snd ins1)
                              . (f $ Bot from)
     in
-        (g rs, (from, fst ins1, snd ins1) : (from, fst ins2, snd ins2) : ins)
+        (g ps, (from, fst ins1, snd ins1) : (from, fst ins2, snd ins2) : ins)
 
-addInstruction :: String -> Robots -> [Instruction] -> (Robots, [Instruction])
-addInstruction s rs ins =
+addInstruction :: String -> Places -> [Instruction] -> (Places, [Instruction])
+addInstruction s ps ins =
     case matchRegex valueGoes s of
     Nothing         -> case matchRegex botGives s of
-                       Nothing  -> (rs, ins)
-                       Just x   -> makeRobotsAndIns x rs ins
-    Just [value, n] -> (maybeAddRobotWithValue (Bot (read n :: Int)) (Just (read value :: Int)) rs, ins)
-    _               -> (rs, ins)
+                       Nothing  -> (ps, ins)
+                       Just x   -> makePlacesAndIns x ps ins
+    Just [value, n] -> (maybeAddRobotWithValue (Bot (read n :: Int)) (Just (read value :: Int)) ps, ins)
+    _               -> (ps, ins)
 
-makeRobots :: Robots -> [Instruction] -> Int -> IO (Robots, [Instruction])
-makeRobots robots ins 0         = return (robots, ins)
-makeRobots robots ins lineCount = do
+makePlaces :: Places -> [Instruction] -> Int -> IO (Places, [Instruction])
+makePlaces robots ins 0         = return (robots, ins)
+makePlaces robots ins lineCount = do
     line <- getLine
-    let (rs, ins') = addInstruction line robots ins
-    makeRobots rs ins' (lineCount - 1)
+    let (ps, ins') = addInstruction line robots ins
+    makePlaces ps ins' (lineCount - 1)
 
-applyValue :: ChipType -> Maybe Int -> Maybe Int -> Robot -> Maybe Robot
-applyValue cType (Just h) (Just l) robot =
+applyValue :: ChipType -> Maybe Int -> Maybe Int -> Place -> Maybe Place
+applyValue cType (Just h) (Just l) place =
     case cType of
-    High -> addValue h robot
-    Low  -> addValue l robot
+    High -> addValue h place
+    Low  -> addValue l place
 
 hasBothChips :: Robot -> Maybe (Maybe Int, Maybe Int)
 hasBothChips r =
@@ -99,22 +112,35 @@ hasBothChips r =
         then Just (h, l)
         else Nothing
 
-runInstructions :: Robots -> [Instruction] -> Robots
-runInstructions rs []      = rs
-runInstructions rs (i@(n, cType, dest):ins) =
+runInstructions :: Places -> [Instruction] -> Places
+runInstructions ps []      = ps
+runInstructions ps (i@(n, cType, dest):ins) =
     case dest of
-    Bot b -> case M.lookup n rs of
-             Nothing    -> runInstructions rs ins
-             Just robot -> case hasBothChips robot of
-                           Nothing     -> runInstructions rs $ ins ++ [i]
-                           Just (h, l) -> runInstructions (M.update (applyValue cType h l) b rs) ins
-    _     -> runInstructions rs ins
+    Bot b    -> case M.lookup (rId n) ps of
+                Nothing    -> runInstructions ps ins
+                Just (PlaceR robot) -> case hasBothChips robot of
+                                       Nothing     -> runInstructions ps $ ins ++ [i]
+                                       Just (h, l) -> runInstructions (M.update (applyValue cType h l) (rId b) ps) ins
+    Output o -> case M.lookup (rId n) ps of
+                Nothing    -> runInstructions ps ins
+                Just (PlaceR robot) -> case hasBothChips robot of
+                                       Nothing     -> runInstructions ps $ ins ++ [i]
+                                       Just (h, l) -> runInstructions (M.update (applyValue cType h l) (bId o) ps) ins
 
-filterRobots :: Int -> Int -> (Int, Robot) -> Bool
-filterRobots h l (_, Robot{ high = rh, low = rl }) =
+filterRobots :: Int -> Int -> (String, Place) -> Bool
+filterRobots h l (_, (PlaceR Robot{ high = rh, low = rl })) =
     h == (fromMaybe 0 rh) && l == (fromMaybe 0 rl)
+filterRobots _ _ _ = False
 
-readLines :: (Int -> IO (Robots, [Instruction])) -> IO (Robots, [Instruction])
+filterBins :: (Int -> Bool) -> (String, Place) -> Bool
+filterBins f (['b', n], (PlaceB _)) = f (read [n] :: Int)
+filterBins _ _                      = False
+
+multBinChips :: (String, Place) -> Int -> Int
+multBinChips (_, (PlaceB n)) total = n * total
+multBinChips _               total = total
+
+readLines :: (Int -> IO (Places, [Instruction])) -> IO (Places, [Instruction])
 readLines method = do
     lineCount <- getLine
     let n = read lineCount :: Int
@@ -122,5 +148,10 @@ readLines method = do
 
 main1 :: IO ()
 main1 = do
-    (rs, ins) <- readLines $ makeRobots M.empty []
-    putStrLn . show $ filter (filterRobots 61 17) $ M.toList $ runInstructions rs ins
+    (ps, ins) <- readLines $ makePlaces M.empty []
+    putStrLn . show $ filter (filterRobots 61 17) $ M.toList $ runInstructions ps ins
+
+main2 :: IO ()
+main2 = do
+    (ps, ins) <- readLines $ makePlaces M.empty []
+    putStrLn . show $ foldr multBinChips 1 $ filter (filterBins (<=2)) $ M.toList $ runInstructions ps ins
